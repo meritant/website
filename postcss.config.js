c3277e7c5c015d9eb0dda5448dad4eaeb4d1f24d
Aleks C <useruser@192.168.1.103>
2024-12-30 18:08:56 +0300

    Deployment on Pages config updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      browsers: [
+          "last 2 versions",
+          "> 1%",
+          "not dead"
+      ]
+  },
   },
 }
